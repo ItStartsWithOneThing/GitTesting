6c3381644b84eba34adbabc4f1b560768f69c918
Konstantin Turkin <like69.kt@gmail.com>
2022-07-05 20:15:36 +0300

    Added Galaxy Fold Class

---
+++ GitTesting/GalaxyFold.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTesting
+{
+    public class GalaxyFold
+    {
+        public string IMEI { get; set; }
+
+        public void Call(int number)
+        {
+            Console.WriteLine("Calling...");
+        }
+    }
+}
