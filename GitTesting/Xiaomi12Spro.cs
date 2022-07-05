d9615f056adc572c53047f88064563e6fb5171d1
Konstantin Turkin <like69.kt@gmail.com>
2022-07-05 20:18:09 +0300

    Implemented Xiaomi12S

---
+++ GitTesting/Xiaomi12Spro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTesting
+{
+    public class Xiaomi12Spro
+    {
+        public string IMEI { get; set; }
+
+        public void Call(int number)
+        {
+            Console.WriteLine("Calling...");
+        }
+
+        public void GetWifi(int password)
+        {
+            Console.WriteLine("Connecting...");
+        }
+
+        public void ReceiveCall(string name)
+        {
+            Console.WriteLine("Receiving...");
+        }
+    }
+}
